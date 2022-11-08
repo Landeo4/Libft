de966001ffab8886b8aebee7106cef03893fd7fc
Landeo4 <theo91.pot@gmail.com>
2022-11-08 23:19:50 +0100

    push

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,20 +6,41 @@
 /*   By: tpotilli@student42.fr  <marvin@42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 13:40:54 by tpotillion        #+#    #+#             */
-/*   Updated: 2022/11/03 19:43:58 by tpotilli@st      ###   ########.fr       */
+/*   Updated: 2022/11/04 15:36:26 by tpotilli@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+char *strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
 
 char *ft_strmapi(char const *s, char (*f) (unsigned int, char))
 {
 	unsigned int i;
 	char *string;
+	int len;
 
+	len = f(i, string + i);
 	i = 0;
-	while (s[i])
-	{
-		s[i] = f(i,string);
-		i++;
-	}
+	s = malloc(sizeof(char) * len);
+	if (s == NULL)
+		return (NULL);
+	ft_strcpy(s, f(i, string));
 	return (s);
 }
